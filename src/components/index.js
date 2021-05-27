1f5c8dfcc27d9457847bc1f107b2b2b46e61e692
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2021-05-27 21:51:42 +0200

    Improve side bar styling

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import About from "./About";
 import Home from "./Home";
 import Navbar from "./TopNavbar";
 import Projects from "./Projects";
-import { Burger } from "./NewNav";
-import { Menu } from "./NewNav";
+import SideBar from "./NewNav";
 
-export { Abilities, About, Home, Navbar, Projects, Burger, Menu };
+export { Abilities, About, Home, Navbar, Projects, SideBar };
