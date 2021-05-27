a5c4e322646536ebf6389b5c6da318b869623d34
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2021-05-27 21:53:23 +0200

    Add on click outside functionality to the sidebar

--- src/hooks.js
+++ src/hooks.js
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 
 export const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
-    const listener = (e) => {
-      if (!ref.current || ref.current.contains(e.target)) {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
-      handler(e);
+      handler(event);
     };
+    document.addEventListener("mousedown", listener);
     return () => {
       document.removeEventListener("mousedown", listener);
     };
